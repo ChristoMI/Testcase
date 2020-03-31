c6fda209a132fc00d20985df7de9df23e2d92489
crescendo <mivaschenko_51@lll.kpi.ua>
2020-03-31 04:59:58 +0300

    database modelling

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
 @app.route('/')
